1287ab193995348a33dbd00053ecec8364bb9d03
__keiitix__ <keiitixlife@gmail.com>
2021-01-15 21:54:31 +0900

    sendgrid2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,6 +10,7 @@ Rails.application.configure do
   config.log_level = :debug
   config.log_tags = [ :request_id ]
   config.action_mailer.perform_caching = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { :host => 'https://klossxcore.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
@@ -21,6 +22,7 @@ Rails.application.configure do
     :authentication => :plain,
     :enable_starttls_auto => true
   }
+  
   config.i18n.fallbacks = true
   config.active_support.deprecation = :notify
   config.log_formatter = ::Logger::Formatter.new
