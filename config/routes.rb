45ab1875e3cf7ee61b2018034881d4be10a6d924
__keiitix__ <keiitixlife@gmail.com>
2021-01-15 17:49:20 +0900

    devise view & controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root 'home#index'
 
-  devise_for :users
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions'   
+  } 
   resources :books
   resources :users
 
