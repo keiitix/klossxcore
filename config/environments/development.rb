0ea9f3b3c7a3111e4936f4c26055b010d993e92b
__keiitix__ <keiitixlife@gmail.com>
2021-01-15 19:25:59 +0900

    devise x gmail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -12,6 +12,16 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :user_name => "unikeiitix@gmail.com",
+    :password => "gickobztkvlldhuo",
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_15_085357) do
+ActiveRecord::Schema.define(version: 2021_01_15_100144) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -33,6 +33,10 @@ ActiveRecord::Schema.define(version: 2021_01_15_085357) do
     t.string "uid"
     t.string "token"
     t.string "meta"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
